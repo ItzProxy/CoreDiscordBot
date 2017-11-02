9880fedefa3d4b6669ce0f5032dc164b3241e5c8
ItzProxy <allure.success@gmail.com>
2017-11-02 12:20:02 -0600

    Added global exceptionextension, removed it form coremusichelper, added functional requirements

---
+++ Core_Discord/CoreExtensions/ExceptionExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core_Discord.CoreExtensions
+{
+    public static class ExceptionExtension
+    {
+        /// <summary>
+        /// Taken from NadekoModuleExtension
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="name"></param>
+        public static void ThrowIfNull<T>(this T obj, string name) where T : class
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(name));
+        }
+    }
+}
--- Core_Discord/CoreMusic/CoreMusicHelper.cs
+++ Core_Discord/CoreMusic/CoreMusicHelper.cs
@@ -11,16 +11,6 @@ namespace Core_Discord.Music
 {
     public static class CoreMusicHelper  
     {
-        /// <summary>
-        /// Taken from NadekoModuleExtension
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="obj"></param>
-        /// <param name="name"></param>
-        public static void ThrowIfNull<T>(this T obj, string name) where T : class
-        {
-            if (obj == null)
-                throw new ArgumentNullException(nameof(name));
-        }
+
     }
 }
