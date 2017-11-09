92fa5b6870082a2871a3b6e14e11dd410cb99c0b
ItzProxy <allure.success@gmail.com>
2017-11-09 11:25:30 -0600

    Small changes

---
+++ Core_Discord/CoreServices/Interfaces/IServiceProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core_Discord.CoreServices.Interfaces
+{
+    public interface IServiceProvider
+    {
+        object GetService(Type serviceType);
+    }
+}
